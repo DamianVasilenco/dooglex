60481b59e409d536fbe4fbf6ec913b8460260f71
Damian Vasilenco <damygaming@gmail.com>
2024-02-24 12:45:39 +0100

    moved code in struct mehod

--- src/main.rs
+++ src/main.rs
@@ -5,49 +5,49 @@ use docx::document::ParagraphContent::Run;
 use docx::document::{BodyContent, RunContent};
 use docx::DocxFile;
 
-fn main() {
-    let args: Vec<_> = env::args().collect();
-    let input_dir = args[1].clone();
-    let string_to_search = args[2].clone();
-    let mut map = HashMap::new();
+struct DoogleX {}
 
-    let paths = fs::read_dir(input_dir).unwrap();
+impl DoogleX {
+    fn search(input_dir: String, string_to_search: String) -> HashMap<String, Vec<String>> {
+        let mut map = HashMap::new();
+        let paths = fs::read_dir(input_dir).unwrap();
 
-    for file in paths {
-        let path = file.unwrap().path();
-        let extension = path.extension().and_then(OsStr::to_str);
+        for file in paths {
+            let path = file.unwrap().path();
+            let extension = path.extension().and_then(OsStr::to_str);
 
-        match extension {
-            None => {continue;}
-            Some(ext) => {
-                if ext != "docx" {
-                    continue;
+            match extension {
+                None => {continue;}
+                Some(ext) => {
+                    if ext != "docx" {
+                        continue;
+                    }
                 }
             }
-        }
 
-        let file_name = path.into_os_string().into_string().unwrap();
-        let docx = DocxFile::from_file(file_name.clone()).unwrap();
-        let docx = docx.parse().unwrap();
+            let file_name = path.into_os_string().into_string().unwrap();
+            let docx = DocxFile::from_file(file_name.clone()).unwrap();
+            let docx = docx.parse().unwrap();
 
-        for content in docx.document.body.content.iter() {
-            if let BodyContent::Paragraph(paragraph) = content {
-                let paragraph_content = paragraph.content.get(0).unwrap();
-                if let Run(run) = paragraph_content {
-                    let run_content = run.content.get(0);
-                    match run_content {
-                        None => {}
-                        Some(content) => {
-                            if let RunContent::Text(text) = content {
-                                let cow = &text.text;
-                                let mut string = String::new();
-                                string.push_str(cow);
+            for content in docx.document.body.content.iter() {
+                if let BodyContent::Paragraph(paragraph) = content {
+                    let paragraph_content = paragraph.content.get(0).unwrap();
+                    if let Run(run) = paragraph_content {
+                        let run_content = run.content.get(0);
+                        match run_content {
+                            None => {}
+                            Some(content) => {
+                                if let RunContent::Text(text) = content {
+                                    let cow = &text.text;
+                                    let mut string = String::new();
+                                    string.push_str(cow);
 
-                                if string.contains(&string_to_search) {
-                                    // println!("{}", file_name);
-                                    // println!("- {}", string);
-                                    // println!("");
-                                    map.entry(String::from(&file_name)).or_insert(Vec::new()).push(String::from(&string));
+                                    if string.contains(&string_to_search) {
+                                        // println!("{}", file_name);
+                                        // println!("- {}", string);
+                                        // println!("");
+                                        map.entry(String::from(&file_name)).or_insert(Vec::new()).push(String::from(&string));
+                                    }
                                 }
                             }
                         }
@@ -55,7 +55,16 @@ fn main() {
                 }
             }
         }
+
+        map
     }
+}
+
+fn main() {
+    let args: Vec<_> = env::args().collect();
+    let input_dir = args[1].clone();
+    let string_to_search = args[2].clone();
+    let map = DoogleX::search(input_dir, string_to_search);
 
     // Print the map
     for (key, value) in &map {
