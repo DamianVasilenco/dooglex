8eb4febb4e4c140e6c065dfa0cd502591b8f08cc
Damian Vasilenco <damygaming@gmail.com>
2024-02-24 12:27:34 +0100

    tmp code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,70 @@
+use std::{env, fs};
+use std::collections::HashMap;
+use std::ffi::OsStr;
+use docx::document::ParagraphContent::Run;
+use docx::document::{BodyContent, RunContent};
+use docx::DocxFile;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<_> = env::args().collect();
+    let input_dir = args[1].clone();
+    let string_to_search = args[2].clone();
+    let mut map = HashMap::new();
+
+    let paths = fs::read_dir(input_dir).unwrap();
+
+    for file in paths {
+        let path = file.unwrap().path();
+        let extension = path.extension().and_then(OsStr::to_str);
+
+        match extension {
+            None => {continue;}
+            Some(ext) => {
+                if ext != "docx" {
+                    continue;
+                }
+            }
+        }
+
+        let file_name = path.into_os_string().into_string().unwrap();
+        let docx = DocxFile::from_file(file_name.clone()).unwrap();
+        let docx = docx.parse().unwrap();
+
+        for content in docx.document.body.content.iter() {
+            if let BodyContent::Paragraph(paragraph) = content {
+                let paragraph_content = paragraph.content.get(0).unwrap();
+                if let Run(run) = paragraph_content {
+                    let run_content = run.content.get(0);
+                    match run_content {
+                        None => {}
+                        Some(content) => {
+                            if let RunContent::Text(text) = content {
+                                let cow = &text.text;
+                                let mut string = String::new();
+                                string.push_str(cow);
+
+                                if string.contains(&string_to_search) {
+                                    // println!("{}", file_name);
+                                    // println!("- {}", string);
+                                    // println!("");
+                                    map.entry(String::from(&file_name)).or_insert(Vec::new()).push(String::from(&string));
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    // Print the map
+    for (key, value) in &map {
+        println!("{}", key);
+
+        for v in value {
+            println!("- {}", v);
+        }
+
+        println!()
+    }
 }
